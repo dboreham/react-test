ba3adfaec58421b42c5e610b470e5918176ee23f
David Boreham <david@bozemanpass.com>
2022-03-09 20:40:52 -0700

    Add button counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Counter value: {count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
